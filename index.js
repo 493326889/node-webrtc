35af898bf45a3e69da63b763de3cae47da5507ec
jiangchangzzz <jiangchangzzz@gmail.com>
2017-08-02 10:43:36 +0800

    添加ICE候选路径处理

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 const ws = require('ws');
+const SemanticSDP = require("semantic-sdp");
+const iceParse=require('wrtc-ice-cand-parse');
+
 const socketPort = 9001;
 const wss = new ws.Server({'port': socketPort});
-let globalConfig = {};
+
+let globalConfig = {
+	remoteSDP: null,
+	remoteCandidate: []
+};
 let remoteIp = '127.0.0.1';
-const SemanticSDP = require("semantic-sdp");
 const serverSDP = require('./sdp');
 let serverSDPObj = SemanticSDP.SDPInfo.process(serverSDP.sdp);
-console.log(serverSDP);
 
 wss.on('connection', (ws) => {
 	console.log('connection');
 	ws.on('message', (data) => {
-		data = JSON.parse(data);
-		if('offer' === data.type) {
-			//  let sdpObj = SemanticSDP.SDPInfo.process(data.sdp).plain();
-			  globalConfig.remoteSDP = data.sdp;
-			  
-			  // send  answer
-				ws.send(JSON.stringify({type: 'answer', sdp: serverSDP.sdp}));
-			//   console.log(sdpObj);
+
+		//解析json字符串
+		try{
+			data = JSON.parse(data);
 		}
-	})
-})
+		catch(e){
+			console.log('Error parsing JSON');
+			data={};
+		}
+		
+		console.log(data.type);
+		switch(data.type){
+			case 'offer':
+				//  let sdpObj = SemanticSDP.SDPInfo.process(data.sdp).plain();
+				console.log('offer',data.sdp);
+				globalConfig.remoteSDP = data.sdp;
+			  
+			  	// send  answer
+				ws.send(JSON.stringify({type: 'answer', sdp: serverSDP.sdp}));
+				//   console.log(sdpObj);
+				break;
+			
+			case 'candidate':
+				console.log('candidate',data.candidate);
+
+				globalConfig.remoteCandidate.push(data.candidate);
+				ws.send(JSON.stringify(data));
+				break;
+			
+			default:
+				console.log('not match');
+				
+				break;
+		}
+	});
+});
